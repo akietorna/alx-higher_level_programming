0c8d6de16c1e27eaa8a024f1ad3456254d93209a
akietorna <akietorna@gmail.com>
2025-10-07 11:26:55 -0700

    Check palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+int is_palindrome(listint_t **head)
+{
+	int a[100];
+	int b = 0;
+	int c = 0;
+	listint_t *end;
+
+	if (*head == NULL)
+	{
+		return (1);
+	}
+	end = *head;
+	while (end != NULL)
+	{
+		a[b] = end->n;
+		end = end->next;
+		b++;
+	}
+	b--;
+	while (c < b)
+	{
+		if (a[c] != a[b])
+		{
+			return (0);
+		}
+		c++;
+		b--;
+	}
+	return (1);
+}
