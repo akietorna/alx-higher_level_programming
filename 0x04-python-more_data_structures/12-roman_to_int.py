a8e301eeb806344f4c439fd9f729fdae5900a24a
akietorna <akietorna@gmail.com>
2025-10-14 00:41:55 -0700

    Convert Roman numerals to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    num = 0
+    i = 0
+    if roman_string == None:
+        return 0
+    while i < len(roman_string):
+        if roman_string[i] == "M":
+            num += 1000
+            i += 1
+        elif roman_string[i] == "D":
+            num += 500
+            i += 1
+        elif i < len(roman_string) - 1 and roman_string[i] == "C" and roman_string[i + 1] == "M":
+            num += 900
+            i += 2
+        elif i < len(roman_string) - 1 and roman_string[i] == "C" and roman_string[i + 1] == "D":
+            num += 400
+            i += 2
+        elif roman_string[i] == "C":
+            num += 100
+            i += 1
+        elif roman_string[i] == "L":
+            num += 50
+            i += 1
+        elif i < len(roman_string) - 1 and roman_string[i] == "X" and roman_string[i + 1] == "C":
+            num += 90
+            i += 2
+        elif i < len(roman_string) - 1 and roman_string[i] == "X" and roman_string[i + 1] == "L":
+            num += 40
+            i += 2
+        elif roman_string[i] == "X":
+            num += 10
+            i += 1
+        elif roman_string[i] == "V":
+            num += 5
+            i += 1
+        elif i < len(roman_string) - 1 and roman_string[i] == "I" and roman_string[i + 1] == "X":
+            num += 9
+            i += 2
+        elif i < len(roman_string) - 1 and roman_string[i] == "I" and roman_string[i + 1] == "V":
+            num += 4
+            i += 2
+        elif roman_string[i] == "I":
+            num += 1
+            i += 1
+    return num
