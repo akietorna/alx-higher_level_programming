9e4e9fe6cf942d75837b8b7943af652193de0931
akietorna <akietorna@gmail.com>
2025-10-14 09:27:01 -0700

    Find weightd average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,7 @@
 def weight_average(my_list=[]):
     score = 0
     avg = 0
-    if my_list == None or my_list == []:
+    if my_list is None or my_list == []:
         return 0
     for a in my_list:
         score += a[0] * a[1]
