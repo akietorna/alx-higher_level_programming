5ecb2bf089e3ec67707bf40a5615443970b7f4a1
akietorna <akietorna@gmail.com>
2025-10-17 05:27:56 -0700

    Delete key-value pair

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,7 +3,7 @@ def complex_delete(a_dictionary, value):
     num = []
     for a, b in a_dictionary.items():
         if b == value:
-           num.append(a)
+            num.append(a)
     if len(num) > 0:
         for i in num:
             a_dictionary.pop(i)
